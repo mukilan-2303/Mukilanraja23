33d769249486ddb10c8a5f3f4fb5c375b9f2c84a
mukilan-2303 <mukilankerenschools@gmail.com>
2025-06-27 19:11:30 +0530

    Update scenario1.sql

--- week2/stored procedures/scenario1.sql
+++ week2/stored procedures/scenario1.sql
@@ -1,1 +1,25 @@
+CREATE TABLE savings_accounts (
+    account_id   NUMBER PRIMARY KEY,
+    customer_id  NUMBER,
+    balance      NUMBER(10, 2)
+);
 
+-- Sample data
+INSERT INTO savings_accounts VALUES (101, 1, 1000);
+INSERT INTO savings_accounts VALUES (102, 2, 2000);
+INSERT INTO savings_accounts VALUES (103, 3, 3000);
+COMMIT;
+
+--process
+CREATE OR REPLACE PROCEDURE ProcessMonthlyInterest IS
+BEGIN
+  UPDATE savings_accounts
+  SET balance = balance + (balance * 0.01);
+  COMMIT;
+END;
+/
+--execute
+EXEC ProcessMonthlyInterest;
+
+-- For Display
+SELECT * FROM savings_accounts;
